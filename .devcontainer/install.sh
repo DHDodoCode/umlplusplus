dcea48e98f3be942c0559768750395c5ba2e770c
Wesley <wesmiller94@live.com>
2024-09-27 21:12:23 +0000

    Update devcontainer with g++ and cmake

---
+++ .devcontainer/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+
+# # Helm Install
+# echo "Installing Helm"
+
+# # Download prebuilt Helm script
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# # Give Exec rights to Helm script
+# chmod 700 get_helm.sh
+# # Run Helm script
+# ./get_helm.sh
+
+# echo "Helm Installed"
+# # Helm Complete
